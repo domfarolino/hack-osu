4b84508b3e1bf1505759cd34788a1d6eec5d88e2
Dom Farolino <domfarolino@gmail.com>
2016-11-19 16:00:39 -0500

    Clean house

--- app.js
+++ app.js
@@ -3,23 +3,24 @@
 require('dotenv').config();
 
 const restify = require('restify');
+const utils = require('./lib/utils');
 const Yelp = require('./lib/apis').Yelp;
 const YELP_CONSUMER_KEY = process.env.YELP_CONSUMER_KEY;
 
 const yelpInstance = new Yelp();
 
-const respond = async (request, response, next) => {
+const yelpFoodAndLocationResponse = async (request, response, next) => {
   try {
     let apiResponse = await yelpInstance.testGetData(request.params.food, request.params.location);
 
     apiResponse = apiResponse.businesses.filter(business => {
       if (!business.is_closed) return business;
     }).map(business => ({
-      'name': business.name,
-      'rating': business.rating,
-      'image_url': business.image_url,
-      'display_address': business.location.display_address,
-      'coordinates': business.location.coordinate
+      name: business.name,
+      rating: business.rating,
+      image_url: business.image_url,
+      display_address: business.location.display_address,
+      coordinates: business.location.coordinate
     }));
 
     response.send(await apiResponse);
@@ -31,8 +32,8 @@ const respond = async (request, response, next) => {
 }
 
 const server = restify.createServer();
-server.get('/api/:food/:location', respond);
-server.head('/api/:food/:location', respond);
+server.get('/api/:food/:location', yelpFoodAndLocationResponse);
+server.head('/api/:food/:location', yelpFoodAndLocationResponse);
 
 server.listen(process.env.SERVE_PORT, () => {
   console.log(`${server.name} listening at ${server.url}`);

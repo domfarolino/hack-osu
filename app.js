78f0b79d6cd26b9e1b10fbac9db23098c2baa3b6
Dom Farolino <domfarolino@gmail.com>
2016-11-19 15:45:44 -0500

    Map + filter

--- app.js
+++ app.js
@@ -10,7 +10,18 @@ const yelpInstance = new Yelp();
 
 const respond = async (request, response, next) => {
   try {
-    const apiResponse = await yelpInstance.testGetData(request.params.food, request.params.location);
+    let apiResponse = await yelpInstance.testGetData(request.params.food, request.params.location);
+
+    apiResponse = apiResponse.businesses.filter(business => {
+      if (!business.is_closed) return business;
+    }).map(business => ({
+      'name': business.name,
+      'rating': business.rating,
+      'image_url': business.image_url,
+      'display_address': business.location.display_address,
+      'coordinates': business.location.coordinate
+    }));
+
     response.send(await apiResponse);
   } catch (apiError) {
     response.send(await apiError);
@@ -20,8 +31,6 @@ const respond = async (request, response, next) => {
 }
 
 const server = restify.createServer();
-server.get('/api/:food', respond);
-server.head('/api/:food', respond);
 server.get('/api/:food/:location', respond);
 server.head('/api/:food/:location', respond);
 

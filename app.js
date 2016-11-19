63e2a814b75e23c020fd3f7ed35c0a19bd2146da
Dom Farolino <domfarolino@gmail.com>
2016-11-19 13:02:29 -0500

    MOVE DOTENV

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict';
 
+require('dotenv').config();
+
 const restify = require('restify');
 const Yelp = require('./lib/apis').Yelp;
+const YELP_CONSUMER_KEY = process.env.YELP_CONSUMER_KEY;
+
+console.log(YELP_CONSUMER_KEY);
 
 const yelpInstance = new Yelp();
 yelpInstance.testGetData().then(response => {console.log(response)}).catch(e => {console.error(e)});
-
-require('dotenv').config();
 
 const respond = (request, response, next) => {
   response.send({ hello: request.params.name });

1ff2862fff21c7c7ea808da1b977f6c77c416f5d
Dom Farolino <domfarolino@gmail.com>
2016-11-19 11:32:02 -0500

    Woo

---
+++ app.js
@@ -0,0 +1,16 @@
+const restify = require('restify');
+
+require('dotenv').config();
+
+function respond(request, response, next) {
+  response.send('hello ' + request.params.name);
+  next();
+}
+
+const server = restify.createServer();
+server.get('/api/:name', respond);
+server.head('/api/:name', respond);
+
+server.listen(process.env.SERVE_PORT, function() {
+  console.log(`${server.name} listening at ${server.url}`);
+});

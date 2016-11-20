e3ebeebfbf1bfff48a1c5502be36f8967b7aeb21
Noah Bass <bassjnoah@gmail.com>
2016-11-19 23:31:54 -0500

    Add MapData

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ require('dotenv').config();
 
 const restify = require('restify');
 const Yelp = require('./lib/apis').Yelp;
-const YELP_CONSUMER_KEY = process.env.YELP_CONSUMER_KEY;
+const MapData = require('./lib/apis/MapData');
 const Utils = require('./lib/utils');
+const Coordinate = require('./lib/Coordinate');
+
+const YELP_CONSUMER_KEY = process.env.YELP_CONSUMER_KEY;
 
 const yelpInstance = new Yelp();
 
@@ -33,12 +36,26 @@ const yelpFoodAndCoordinatesResponse = async (request, response, next) => {
   next();
 }
 
-const server = restify.createServer();
-server.get('/api/:food/:location', yelpFoodAndLocationResponse);
-server.head('/api/:food/:location', yelpFoodAndLocationResponse);
+const mapData = async (request, response, next) => {
+  try {
+    const userCoordinate = new Coordinate(request.params.lat, request.params.long);
+    const mapData = new MapData(userCoordinate, request.params.userLoationName, request.params.foodCategory);
+    const data = await mapData.getData();
+    response.send(data);
+  } catch(error) {
+    response.send(error);
+  }
 
-server.get('/api/:food/:lat/:long', yelpFoodAndCoordinatesResponse);
-server.head('/api/:food', yelpFoodAndCoordinatesResponse);
+  next();
+}
+
+const server = restify.createServer();
+server.get('/api/:foodCategory/:userLoationName/:lat/:long', mapData);
+// server.get('/api/:food/:location', yelpFoodAndLocationResponse);
+// server.head('/api/:food/:location', yelpFoodAndLocationResponse);
+
+// server.get('/api/:food/:lat/:long', yelpFoodAndCoordinatesResponse);
+// server.head('/api/:food', yelpFoodAndCoordinatesResponse);
 
 server.listen(process.env.SERVE_PORT, () => {
   console.log(`${server.name} listening at ${server.url}`);
